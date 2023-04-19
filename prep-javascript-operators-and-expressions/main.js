a9b7b0b65759aaa70feb3f82aa1260c396d0f71d
Kyle Kettler <hello@kylekettler.com>
2023-04-19 03:45:07 +0000

    Completed prep-javascript-operators-and-expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,53 @@
+// Task 6.i
+const number1 = 3;
+const number2 = 5;
+const product = number1 * number2;
+
+console.log(product);
+console.log(typeof product);
+
+// Task 6.ii
+const charge = 500;
+const payment = 200;
+const amountRemaining = charge - payment;
+
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+// Task 6.iii
+const tests = 82;
+const assignments = 91;
+const final = 89;
+const grade = (tests + assignments + final) / 3;
+
+console.log(grade);
+console.log(typeof grade);
+
+// Task 6.iv
+const firstName = 'Kyle';
+const lastName = 'Kettler';
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
+console.log(typeof fullName);
+
+// Task 6.v
+const pH = 5;
+const isAcidic = pH < 7;
+
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+// Task 6.vi
+const numberOfSoldiers = 300;
+const isSparta = numberOfSoldiers === 300;
+
+console.log(isSparta);
+console.log(typeof isSparta);
+
+// Task 6.vii
+let nickname = fullName;
+nickname += ' is the GOAT';
+
+console.log(nickname);
+console.log(typeof nickname);
