7e97541dd267b7090bf28deecc46fa5606bf11ac
Kyle Kettler <hello@kylekettler.com>
2023-04-22 15:41:30 +0000

    completed prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,73 @@
+// addTwoNumbers Exercise
+function addTwoNumbers(a, b) {
+  return a + b;
+}
+
+const addTwoNumbersResult = addTwoNumbers(2, 2);
+console.log('addTwoNumbers Exercise:', addTwoNumbersResult);
+
+// convertHoursToMinutes Exercise
+function convertHoursToMinuts(hours) {
+  return hours * 60;
+}
+
+const convertHoursToMinutsResults = convertHoursToMinuts(2);
+console.log('convertHoursToNumbers Exercise:', convertHoursToMinutsResults);
+
+// getGreeting Exercise
+function getGreeting(name) {
+  const greeting = 'Hello ' + name + '!';
+  return greeting;
+}
+
+const getGreetingResults = getGreeting('World');
+console.log('getGreeting Exercise:', getGreetingResults);
+
+// addAndMultiplyBy5 Exercise
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+const addAndMultiplyBy5Results = addAndMultiplyBy5(10, 5);
+console.log('addAndMultiplyBy5 Excercise:', addAndMultiplyBy5Results);
+
+// multiplyAndDivideBy5 Exercise
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+
+const multiplyAndDivideBy5Results = multiplyAndDivideBy5(35, 10);
+console.log('multiplyAndDivideBy5 Exercise:', multiplyAndDivideBy5Results);
+
+// subtractTwoNumbers Exercise
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+const subtractTwoNumbersResults = subtractTwoNumbers(22, 7);
+console.log('subtractTwoNumbers Exercise:', subtractTwoNumbersResults);
+
+// getCircleCircumference Exercise
+function getCircleCircumference(radius) {
+  const pie = 3.141592653589793;
+  return 2 * pie * radius;
+}
+
+const getCircleCircumferenceResults = getCircleCircumference(5);
+console.log('getCircleCircumference Exercise:', getCircleCircumferenceResults);
+
+// getFullName Exercise
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+const getFullNameResults = getFullName('Juan', 'Ramirez');
+console.log('getFullName Exercise:', getFullNameResults);
+
+// cube Exercise
+function cube(number) {
+  return number * number * number;
+}
+
+const cubeResults = cube(5);
+console.log('cube Exercise:', cubeResults);
---
+++ prep-javascript-functions/prep-javascript-functions-quiz-notes.md
@@ -0,0 +1,47 @@
+# prep-javascript-functions-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What are the five parts of a function definition?
+
+  1. Function keyword
+  2. An optional name of the funciton
+  3. A comma-separated list of parameters surrounded by parentheses\
+  4. Opening and closing curly braces
+  5. The code of the function inside the curly braces
+
+- How do you call a function?
+  You call a function by declaring the name of it with parentheses at the end of it. You can pass arguments into the parentheses if needed.
+
+- What is the difference between a parameter and an argument?
+  When we define a function, we declare parameters and that when we call a function, we pass it arguments.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
